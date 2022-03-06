d762347c2d9978920f2e6826595341d8842522ca
SasiniD <sasidesilvaabc@gmail.com>
2022-03-07 02:25:04 +0530

    Interpolation - variable binding

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
+  public firstProductItemName = "White Basmathi Rice" ;
   constructor() { }
 
   ngOnInit(): void {
