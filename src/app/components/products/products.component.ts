82a5b21ce2d8596ace8cf5a50ab7997f311fcd22
SasiniD <sasidesilvaabc@gmail.com>
2022-03-07 02:39:18 +0530

    Directives -ngClass

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -6,27 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-  public firstProductItemName = "White Basmathi Rice" ;
-  constructor() { }
+  public firstProductItemName = 'White Basmathi Rice';
+  public storage = 10;
+  public isLowInventory = true;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
-  public getPriceofRice(){
+  ngOnInit(): void {}
+
+  public getPriceofRice() {
     return 350;
   }
 
-  // variables related to show how private protected members work in interpolation.
-  // comment out and try
-  //private firstProductItemName = "White Basmathi Rice" ;
-  //protected firstProductItemName = "White Basmathi Rice" ;
-
-
-
-  // variables related to show Invalid templates using Assignment, bitwise, increment and decrement perators (=,+=, -=, |, &, ++, –,!, ?. etc.)
-  // comment out and try
-  // public quantityOfRice : number = 5;
-  //<td>{{quantityOfRice++}}</td>
-  //<td>{{quantityOfRice + 1 }}</td>
-
+  public getStorage() {
+    if (this.storage < 50) {
+      this.isLowInventory = true;
+    }
+    return this.storage;
+  }
 }
