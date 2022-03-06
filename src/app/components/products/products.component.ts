8a16cce1708aac0efc9fcce9197b77936ee550d2
SasiniD <sasidesilvaabc@gmail.com>
2022-03-07 03:52:42 +0530

    Services - Removed hardcoded product data

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/Models/product.model';
 
 @Component({
   selector: 'app-products',
@@ -12,41 +13,7 @@ export class ProductsComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-  public products = [
-    {
-      productId: '001',
-      productName: 'White Basmathi Rice',
-      createdDate: '2020.01.29',
-      quantity: 100,
-      unitPrice: '400',
-      productDescription:
-        'White Basmathi Rice imported from pakistan. High quality rice with extra fragnance. Organically grown.',
-    },
-    {
-      productId: '002',
-      productName: 'Sugar',
-      createdDate: '2020.01.29',
-      quantity: 1200,
-      unitPrice: '200',
-      productDescription: 'White sugar manufactured by Palwatte Factory',
-    },
-    {
-      productId: '003',
-      productName: 'Flour',
-      createdDate: '2020.01.29',
-      quantity: 50,
-      unitPrice: '190',
-      productDescription: 'Super FIne Whole grain general Purpose flour ',
-    },
-    {
-      productId: '004',
-      productName: 'Dhal',
-      createdDate: '2020.01.29',
-      quantity: 10,
-      unitPrice: '200',
-      productDescription: 'Imported mysoor dhal from India',
-    },
-  ];
+  public products: Product[] = [];
 
   public selectProduct(selectedRow: number) {
     this.rowIndex = selectedRow;
