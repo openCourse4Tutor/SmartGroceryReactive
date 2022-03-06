5f8a2247578c63b9c2d4c1470a7f439ffec5e406
SasiniD <sasidesilvaabc@gmail.com>
2022-03-07 03:28:14 +0530

    Reactive form - .ts file implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ContentLimitterPipe } from './Pipe/content-limitter.pipe';
 import { AddProductComponent } from './components/add-product/add-product.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AddProductComponent } from './components/add-product/add-product.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
@@ -6,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-product.component.css'],
 })
 export class AddProductComponent implements OnInit {
-  constructor() {}
+  productFrom = this.fb.group({
+    productName: ['', Validators.required],
+    description: ['', Validators.required],
+    category: ['', Validators.required],
+    brand : ['', Validators.required],
+    expiredDate: ['', Validators.required],
+    manufacturedDate: ['', Validators.required],
+    batchNumber: ['', Validators.required],
+    unitPrice: ['', [Validators.required, Validators.min(1)]],
+    quantity: ['', [Validators.required, Validators.min(50)]],
+    createdDate: ['', Validators.required],
+  });
+
+  isDataUploading = false;
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  onSubmit() {}
 }
