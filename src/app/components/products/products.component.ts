0014aa50dc87d289426657e678c8f80b35c1cec9
SasiniD <sasidesilvaabc@gmail.com>
2022-03-07 02:44:40 +0530

    Directives - ngFor

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -6,22 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-  public firstProductItemName = 'White Basmathi Rice';
-  public storage = 10;
-  public isLowInventory = true;
-
   constructor() {}
 
   ngOnInit(): void {}
-
-  public getPriceofRice() {
-    return 350;
-  }
-
-  public getStorage() {
-    if (this.storage < 50) {
-      this.isLowInventory = true;
-    }
-    return this.storage;
-  }
+  public products = [
+    {
+      productId: '001',
+      productName: 'White Basmathi Rice',
+      createdDate: '2020.01.29',
+      quantity: '100',
+      unitPrice: '400',
+      productDescription: 'White Basmathi Rice imported from pakistan',
+    },
+    {
+      productId: '002',
+      productName: 'Sugar',
+      createdDate: '2020.01.29',
+      quantity: '1200',
+      unitPrice: '200',
+      productDescription: 'White sugar manufactured by Palwatte Factory',
+    },
+    {
+      productId: '003',
+      productName: 'Flour',
+      createdDate: '2020.01.29',
+      quantity: '50',
+      unitPrice: '190',
+      productDescription: 'Super FIne Whole grain general Purpose flour ',
+    },
+    {
+      productId: '004',
+      productName: 'Dhal',
+      createdDate: '2020.01.29',
+      quantity: '10',
+      unitPrice: '200',
+      productDescription: 'Imported mysoor dhal from India',
+    },
+  ];
 }
