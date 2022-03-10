6c17a3d6c7839fff7dcd52edd62e405d5290b9cd
openCourse4Tutor <100123429+openCourse4Tutor@users.noreply.github.com>
2022-03-10 18:48:36 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
-import { ContentLimitterPipe } from './Pipe/content-limitter.pipe';
 import { AddProductComponent } from './components/add-product/add-product.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { EditProductComponent } from './components/edit-product/edit-product.component';
-import { ViewProductDetailsComponent } from './components/view-product-details/view-product-details.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    ContentLimitterPipe,
-    AddProductComponent,
-    EditProductComponent,
-    ViewProductDetailsComponent
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
