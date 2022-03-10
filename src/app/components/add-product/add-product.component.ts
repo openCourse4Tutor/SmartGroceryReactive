0467c24c24b0df6f6211b01ddf820cdae402873c
openCourse4Tutor <opencourse4tutor@codl.lk>
2022-03-10 19:14:29 +0530

    4 files modified

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -1,14 +1,19 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { Product } from 'src/app/Models/product.model';
-import { ProductService } from 'src/app/Services/product.service';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.css'],
+  styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
+
+  ngOnInit(): void {}
+
+  onSubmit() {}
+
+  constructor(private fb: FormBuilder) {}
+
   productFrom = this.fb.group({
     productName: ['', Validators.required],
     description: ['', Validators.required],
@@ -19,33 +24,10 @@ export class AddProductComponent implements OnInit {
     batchNumber: ['', Validators.required],
     unitPrice: ['', [Validators.required, Validators.min(1)]],
     quantity: ['', [Validators.required, Validators.min(50)]],
+    createdDate: ['', Validators.required],
   });
 
-  isDataUploading = false;
-  @Output() cancelAddView :EventEmitter<void> = new EventEmitter<void>();
-  @Output() productAddEvent :EventEmitter<void> = new EventEmitter<void>();
-
-  constructor(private fb: FormBuilder,
-    private productService :ProductService) {}
-
-  ngOnInit(): void {}
-
-   get f() {
+  get f() {
     return this.productFrom.controls;
   }
-
-  onSubmit() {
-    const values = this.productFrom.value as Product;
-    values.createdDate = new Date().toDateString();
-    this.isDataUploading = true;
-    this.productService.addProduct(values as Product).subscribe((res) => {
-      this.productAddEvent.emit();
-      this.isDataUploading = false;
-      this.productFrom.reset();
-    });
-  }
-
-  cancel(){
-    this.cancelAddView.emit();
- }
 }
