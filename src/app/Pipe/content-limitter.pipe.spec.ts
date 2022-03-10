9d9fe63cbec9668cbc1f6c5ab7454f4c21db65ad
openCourse4Tutor <100123429+openCourse4Tutor@users.noreply.github.com>
2022-03-10 18:46:32 +0530

    Delete src/app/Pipe directory

--- src/app/Pipe/content-limitter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ContentLimitterPipe } from './content-limitter.pipe';
-
-describe('ContentLimitterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ContentLimitterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
