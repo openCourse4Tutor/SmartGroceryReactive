8ece226d3a5cfdadd1caf6ada997b34324725b07
openCourse4Tutor <opencourse4tutor@codl.lk>
2022-03-12 05:48:10 +0530

    2 files modified

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,71 +1,17 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Product } from 'src/app/Models/product.model';
-import { ProductService } from 'src/app/Services/product.service';
-import { ViewProductDetailsComponent } from '../view-product-details/view-product-details.component';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
-})
+ })
 export class ProductsComponent implements OnInit {
-  public rowIndex!: number;
-  showAddProduct!: boolean;
-  isLoading :boolean = false;
-  showEditProduct!: boolean;
-  selectedProduct! :Product;
-  message! :string;
-  public products: Product[] = [];
-  constructor(private productService : ProductService) {}
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.getProducts();
   }
-
-  public selectProduct(selectedRow:any, product :Product) {
-    this.rowIndex = selectedRow;
-    this.selectedProduct =product;
-  }
-
-  showAddProducts() {
-    if(this.showEditProduct){
-      this.showEditProduct =false;
-    }
-    this.showAddProduct = true;
-  }
-
-  hideAddProducts() {
-    this.showAddProduct = false;
-  }
-
-  refresh() {
-    this.getProducts();
-  }
-
-  getProducts() {
-    this.isLoading = true;
-    this.productService.getProducts().subscribe((res) => {
-      this.products = res.data;
-      this.isLoading = false;
-    });
-  }
-
-  OpenEditProductView(){
-    if(this.showAddProduct){
-      this.showAddProduct = false;
-    }
-    this.showEditProduct = true;
-  }
-
-  closeEditView(){
-    this.showEditProduct = false;
-  }
-
-  updateProductList(){
-    this.getProducts();
-  }
-}
-function ViewProductComponent(ViewProductComponent: any) {
-  throw new Error('Function not implemented.');
+  
+    
 }
 
